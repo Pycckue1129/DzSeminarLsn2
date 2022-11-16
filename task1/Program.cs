da45f086d929ce1eeec586c104a99162b74e3b7c
Artem <burkun_artem@mail.ru>
2022-11-16 10:48:49 +0300

    add all tasks

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) % 100 / 10);
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int rezervNum = num;
+int i = 0;
+int i2 = 0;
+while (num != 0)
+{
+    num = num / 10;
+    i++;
+}
+
+if (i > 2)
+{
+    while (i2 != i - 3)
+    {
+      rezervNum = rezervNum / 10;
+       i2++;
+    }
+    Console.WriteLine(rezervNum % 10);
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
